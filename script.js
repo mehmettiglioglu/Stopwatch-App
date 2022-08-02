9e50f0ac84694fd49ad02dc4f0d519ee702746cd
Mehmet Tığlıoğlu <105611803+mehmettiglioglu@users.noreply.github.com>
2022-08-03 00:14:35 +0300

    updated

--- script.js
+++ script.js
@@ -1,34 +1,80 @@
-let wordsDOM=document.querySelector("#words");
-let typeDOM=document.querySelector("#type");
-let scoreDOM=document.querySelector("#score");
+let sec=document.querySelector("#sec");
+let min=document.querySelector("#min");
+let hour=document.querySelector("#hours")
 
-let knownWords=0;
+let start=document.querySelector("#start");
+let pause=document.querySelector("#pause");
+let reset=document.querySelector("#reset");
+let bring=document.querySelector("#bring")
 
+let second=0;
+let minute=0;
+let hours=0;
+let interval=null;
 
-
-wordsDOM.value=["selam","merhaba","dünya"]
-let newArray=wordsDOM.value.split(",")
-
-
-
-typeDOM.addEventListener("input",checkWords);
-
-
-
-function checkWords()
-{
-    for(let i=0;i<newArray.length;i++)
-    {
-        
-        if(newArray[i]===typeDOM.value){
-            typeDOM.value="";
-            console.log(wordsDOM.value.split(",")[0].style.color='green')
-            knownWords++;
+start.addEventListener("click",()=>{
+   if(interval!==null){
+    clearInterval(interval)
+   }
+  interval=setInterval(() => {
+        second++;
+        if(second<10)
+        {
+            sec.innerHTML="0"+second;
         }
-    }
-     scoreDOM.innerHTML =` <h2> Score : ${knownWords} `
-}
+        else
+        {
+            sec.innerHTML=second;
+        }
+        if(second==60)
+        {
+            second=0;
+            minute++;
+            min.innerHTML=minute<10? "0"+minute : minute;
+            if(minute==60)
+            {
+                minute=0;
+                hours++
+                hour.innerHTML=hours<10?"0"+hours :hours;
+            }
+            
+        }
+        
+   document.title=`${hours<10?"0"+hours:hours}:${minute<10?"0"+minute:minute}:${second<10?"0"+second:second}`
+   
+    }, 10);
+})
 
+pause.addEventListener("click",()=>{
+    clearInterval(interval)
 
+    localStorage.setItem("second",JSON.parse(second));
+    localStorage.setItem("minute",JSON.parse(minute));
+    localStorage.setItem("hour",JSON.parse(hours));
+})
 
+reset.addEventListener("click",()=>{
+    clearInterval(interval)
+    localStorage.setItem("second",JSON.parse(second));
+    localStorage.setItem("minute",JSON.parse(minute));
+    localStorage.setItem("hour",JSON.parse(hours));
+    
+    second=0;
+    minute=0;
+    hour=0;
 
+    sec.innerHTML="00";
+    min.innerHTML="00";
+    hour.innerHTML="00"
+})
+
+bring.addEventListener("click",()=>{
+    second=localStorage.getItem("second");
+    minute=localStorage.getItem("minute");
+    hours=localStorage.getItem("hour");
+
+    sec.innerHTML=second<10?"0"+second:second;
+    min.innerHTML=minute<10?"0"+minute:minute;
+    hour.innerHTML=hours<10?"0"+hours:hours;
+
+})
